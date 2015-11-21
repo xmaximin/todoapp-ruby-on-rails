faa9563b409afabe809b92797f01f29408a1bd78
xmaximin <xmaximin@gmail.com>
2015-11-21 04:23:51 +0100

    Mark As Complete Ability

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120142630) do
+ActiveRecord::Schema.define(version: 20151121003542) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
     t.integer  "todo_list_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "complted_at"
+    t.datetime "completed_at"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
